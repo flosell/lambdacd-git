7017ac88ab57baf6600c7d38823d2566c789f08f
Florian Sellmayr <florian.sellmayr@gmail.com>
2016-01-01 16:24:59 +0100

    clean up naming in test

--- test/lambdacd_git/core_test.clj
+++ test/lambdacd_git/core_test.clj
@@ -41,7 +41,7 @@
                                                                                         (fn [args ctx]
                                                                                           (wait-for-git ctx (get-in @state [:git :remote]) "master" :ms-between-polls 100)))]
                                     (first (vals (:outputs execute-step-result)))))]
-    (swap! state #(assoc % :wait-for-result-channel wait-for-result-channel))
+    (swap! state #(assoc % :result-channel wait-for-result-channel))
     state))
 
 (defn start-clone-step [state ref cwd]
@@ -50,7 +50,7 @@
                                                                                         (fn [args ctx]
                                                                                           (clone ctx (get-in @state [:git :remote]) ref cwd)))]
                                     (first (vals (:outputs execute-step-result)))))]
-    (swap! state #(assoc % :wait-for-result-channel wait-for-result-channel))
+    (swap! state #(assoc % :result-channel wait-for-result-channel))
     state))
 
 (defn read-channel-or-time-out [c & {:keys [timeout]
@@ -65,8 +65,8 @@
 
 (defn get-step-result [state]
   (try
-    (let [result (read-channel-or-time-out (:wait-for-result-channel @state))]
-      (swap! state #(assoc % :wait-for-git-result result))
+    (let [result (read-channel-or-time-out (:result-channel @state))]
+      (swap! state #(assoc % :step-result result))
       state)
     (catch Exception e
       (kill-waiting-step state)
@@ -83,8 +83,8 @@
 (defn commit-hash-by-msg [state msg]
   (git-utils/commit-by-msg (:git @state) msg))
 
-(defn wait-for-git-result [state]
-  (:wait-for-git-result @state))
+(defn step-result [state]
+  (:step-result @state))
 
 (deftest wait-for-git-test-clean
   (testing "that it waits for a new commit to happen and that it prints out information on old and new commit hashes"
@@ -95,10 +95,10 @@
                     (wait-a-bit)
                     (git-commit "other commit")
                     (get-step-result))]
-      (is (= :success                               (:status (wait-for-git-result state))))
-      (is (= (commit-hash-by-msg state "initial commit") (:old-revision (wait-for-git-result state))))
-      (is (= (commit-hash-by-msg state "other commit") (:revision     (wait-for-git-result state))))
-      (is (str-containing (commit-hash-by-msg state "initial commit") (:out (wait-for-git-result state))))))
+      (is (= :success (:status (step-result state))))
+      (is (= (commit-hash-by-msg state "initial commit") (:old-revision (step-result state))))
+      (is (= (commit-hash-by-msg state "other commit") (:revision     (step-result state))))
+      (is (str-containing (commit-hash-by-msg state "initial commit") (:out (step-result state))))))
   (testing "that it prints out information on old and new commit hashes"
     (let [state (-> (init-state)
                     (git-init)
@@ -107,8 +107,8 @@
                     (wait-a-bit)
                     (git-commit "other commit")
                     (get-step-result))]
-      (is (str-containing (commit-hash-by-msg state "initial commit") (:out (wait-for-git-result state))))
-      (is (str-containing (commit-hash-by-msg state "other commit") (:out (wait-for-git-result state))))))
+      (is (str-containing (commit-hash-by-msg state "initial commit") (:out (step-result state))))
+      (is (str-containing (commit-hash-by-msg state "other commit") (:out (step-result state))))))
   (testing "that waiting returns immediately when a commit happened while it was not waiting"
     (let [state (-> (init-state)
                     (git-init)
@@ -120,9 +120,9 @@
                     (git-commit "commit while not waiting")
                     (start-wait-for-git-step)
                     (get-step-result))]
-      (is (= :success                                           (:status (wait-for-git-result state))))
-      (is (= (commit-hash-by-msg state "other commit") (:old-revision (wait-for-git-result state))))
-      (is (= (commit-hash-by-msg state "commit while not waiting") (:revision     (wait-for-git-result state))))))
+      (is (= :success (:status (step-result state))))
+      (is (= (commit-hash-by-msg state "other commit") (:old-revision (step-result state))))
+      (is (= (commit-hash-by-msg state "commit while not waiting") (:revision     (step-result state))))))
   (testing "that wait-for can be killed and that the last seen revision is being kept"
     (let [state (-> (init-state)
                     (git-init)
@@ -131,8 +131,8 @@
                     (wait-a-bit)
                     (kill-waiting-step)
                     (get-step-result))]
-      (is (= :killed                                (:status (wait-for-git-result state))))
-      (is (= (commit-hash-by-msg state "initial commit") (:_git-last-seen-revision (wait-for-git-result state))))))
+      (is (= :killed (:status (step-result state))))
+      (is (= (commit-hash-by-msg state "initial commit") (:_git-last-seen-revision (step-result state))))))
   (testing "that it retries until being killed if the repository cannot be reached"
     (let [state (-> (init-state)
                     (set-git-remote "some-uri-that-doesnt-exist")
@@ -140,7 +140,7 @@
                     (wait-a-bit)
                     (kill-waiting-step)
                     (get-step-result))]
-      (is (= :killed (:status (wait-for-git-result state))))))
+      (is (= :killed (:status (step-result state))))))
   (testing "that it prints out errors if a repository can't be reached"
     (let [state (-> (init-state)
                     (set-git-remote "some-uri-that-doesnt-exist")
@@ -148,7 +148,7 @@
                     (wait-a-bit)
                     (kill-waiting-step)
                     (get-step-result))]
-      (is (str-containing "some-uri-that-doesnt-exist" (:out (wait-for-git-result state)))))))
+      (is (str-containing "some-uri-that-doesnt-exist" (:out (step-result state)))))))
 
 (deftest clone-test
   (testing "that we can clone a specific commit"
@@ -162,7 +162,7 @@
           (git-commit "second commit")
           (start-clone-step (commit-hash-by-msg state "first commit") workspace)
           (get-step-result))
-      (is (= :success (:status (wait-for-git-result state))))
+      (is (= :success (:status (step-result state))))
       (is (= "some content"
              (slurp (io/file workspace "some-file"))))))
   (testing "that we can get information on the progress of a clone"
@@ -174,7 +174,7 @@
           (git-commit "some commit")
           (start-clone-step (commit-hash-by-msg state "some commit") workspace)
           (get-step-result))
-      (is (str-containing "Receiving" (:out (wait-for-git-result state))))))
+      (is (str-containing "Receiving" (:out (step-result state))))))
   (testing "that we get a proper error if a commit cant be found"
     (let [state (init-state)
           workspace (util/create-temp-dir)]
@@ -184,5 +184,5 @@
           (git-commit "some commit")
           (start-clone-step "some-ref" workspace)
           (get-step-result))
-      (is (= :failure (:status (wait-for-git-result state))))
-      (is (str-containing "Could not find ref some-ref" (:out (wait-for-git-result state)))))))
+      (is (= :failure (:status (step-result state))))
+      (is (str-containing "Could not find ref some-ref" (:out (step-result state)))))))
