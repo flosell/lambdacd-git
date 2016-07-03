f35717c3af4a6a7d95c8d8c1e8601c101c6eb8d0
Florian Sellmayr <florian.sellmayr@gmail.com>
2016-07-03 12:35:14 +0200

    Version 0.1.5

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
 
 (println "Building against LambdaCD version" lambdacd-version)
 
-(defproject lambdacd-git "0.1.5-SNAPSHOT"
+(defproject lambdacd-git "0.1.5"
   :description "Git support for LambdaCD"
   :url "https://github.com/flosell/lambdacd-git"
   :license {:name "Apache License, version 2.0"
